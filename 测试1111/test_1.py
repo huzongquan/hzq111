1c02fdd1164701720c72da96a03080f5ec2da61b
864119185 <864119185@qq.com>
2019-10-08 15:20:42 +0800

    delete file1

---
+++ 测试1111/test_1.py
@@ -0,0 +1,10 @@
+# -*- coding：utf-8 -*-
+# @Time  : 2019/10/8 15:19
+# @Author: huzongquan
+# @File  : test_1.py
+# @Describe :this is describe
+import time
+
+for i in range(100):
+    print(i)
+    time.sleep(1)
