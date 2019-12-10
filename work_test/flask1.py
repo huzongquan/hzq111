00450b414cb9f162b7328826bd07a1d02c5e49b6
864119185 <864119185@qq.com>
2019-12-10 19:35:34 +0800

    test rebase

--- work_test/flask1.py
+++ work_test/flask1.py
@@ -3,8 +3,7 @@ import sys
 def test1():
     print('aaaa')
 def test2(params):
-    s = params.loads()
-    print(s.get('a'))
+    print(params)
 if __name__ == '__main__':
     test1()
     params = sys.argv[1]
